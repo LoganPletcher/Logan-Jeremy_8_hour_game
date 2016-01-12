23c7577c16c9554e7257582ec647b4a51e640c11
LoganPletcher <lpletch95@hotmail.com>
2016-01-12 12:53:21 -0600

    Logan Commit 2
    Arrow half complete.

---
+++ Assets/Arrow_Collision_Point.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Arrow_Collision_Point
+{
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
